66e2992b525581f4bbf67926d132864b327a07e3
Laura <lauraalspaugh2002@icloud.com>
2024-05-21 10:59:38 -0400

    passing state to props

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import logo from './logo.svg';
 import React from 'react';
 import memesData from './memesData.js';
 import './App.css';
+import Header from './Header.jsx';
 
 function App() {
- 
+ const [user, setUser] = React.useState("Matt")
   
-  const [memeImage, setMemeImage] = React.useState("")
+  const [memeImage, setMemeImage] = React.useState({
+    topText: "",
+          bottomText: "",
+          randomImage: "http://i.imgflip.com/1bij.jpg" 
+  })
   function getMemeImage() {
     const memesArray = memesData.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    setMemeImage(memesArray[randomNumber].url)
+    const url = memesArray[randomNumber].url
+    setMemeImage(prevMeme => ({
+        ...prevMeme,
+        randomImage: url
+    }))
   }
   return (
     <div className="App">
@@ -18,13 +27,18 @@ function App() {
         <h1><i className='mdi mdi-wizard-hat'></i>Meme Generator</h1>
         <h4>React Course - Project 3</h4>
       </header>
+      <div>
+        <h2>
+<Header user = {user}/>
+        </h2>
+      </div>
       <div className='input-fields'>
 <input placeholder='Top Text....'></input>
 <input placeholder='Bottom Text...'></input>
       </div>
       <button onClick={getMemeImage} className='btn meme-button'>Get a new meme image <i className='mdi mdi-panorama-variant'></i></button>
       <div>
-        <img className='meme-image' alt='meme' src={memeImage}></img>
+        <img className='meme-image' alt='meme' src={memeImage.randomImage}></img>
       </div>
     
     </div>
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+export default function Header(props){
+    return(
+        <main>
+<h3>
+   Hello {props.user}. Welcome to Meme Generator. 
+</h3>
+        </main>
+    )
+}
